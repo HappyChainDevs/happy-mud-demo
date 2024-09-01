7c642d382a0d4cf23a8298ddaa288502e5868179
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-09-01 17:00:00 +0800

    Step 6: Integrate HappyChain

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,30 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"
 import { ToastContainer } from "react-toastify";
+import { useHappyChain } from "@happychain/react";
+import { custom } from "viem";
 import { LoadingWrapper } from "./LoadingWrapper";
-import { createBurnerWalletClient } from "./mud/account";
+import { createEmojimonWalletClient } from "./mud/account";
 import { useDevTools } from "./mud/devTools";
 import { useFaucet } from "./mud/faucet";
 import { useSetup } from "./mud/setup";
 import { WalletClientWithAccount } from "./mud/types";
 import { MUDProvider } from "./MUDContext";
 
-const walletClient = createBurnerWalletClient()
+function happyConnectStub() {}
+function happyDisconnectStub() {}
 
 export const App = () => {
   const [wallet, setWallet] = useState<WalletClientWithAccount | undefined>();
+  const { user, provider } = useHappyChain();
   const mud = useSetup(wallet);
   useFaucet(wallet?.account.address);
   useDevTools(mud);
 
-  const connect = () => setWallet(walletClient);
-  const disconnect = () => setWallet(undefined);
+  useEffect(() => {
+    if (!user || !provider) return;
+    setWallet(createEmojimonWalletClient(user.address, custom(provider)));
+  }, [user, provider]);
 
   return mud
     ? <MUDProvider value={mud}>
         <div style={{ position: "absolute", top: "0", right: "0", padding: "20px" }}>
           {!wallet
-            ? <button onClick={connect} style={{ cursor: "pointer" }}>Connect</button>
-            : <button onClick={disconnect} style={{ cursor: "pointer" }}>Disconnect</button>}
+            ? <button onClick={happyConnectStub} style={{ cursor: "pointer" }}>Connect</button>
+            : <button onClick={happyDisconnectStub} style={{ cursor: "pointer" }}>Disconnect</button>}
         </div>
         <LoadingWrapper/>;
         <ToastContainer position="bottom-right" draggable={false} theme="dark"/>
