bb4ca6ca286a056d525c9c87f6ea5d9a4ffa51fd
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-09-01 17:00:00 +0800

    Step 6: Integrate HappyChain

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,9 +2,13 @@ import "tailwindcss/tailwind.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import ReactDOM from "react-dom/client";
+import { HappyWalletProvider } from "@happychain/react";
 import { App } from "./App"
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(
+  <HappyWalletProvider>
+    <App />
+  </HappyWalletProvider>);
