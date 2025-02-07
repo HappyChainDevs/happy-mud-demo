802e1b6891bca46c5faecf8d7493b540bc95afc6
ultraviolet10 <aritrac1998@hotmail.com>
2025-02-07 16:41:09 +0530

    remove comment

--- packages/client/src/mud/devTools.ts
+++ packages/client/src/mud/devTools.ts
@@ -33,7 +33,6 @@ async function mountDevTools(mud: MUDInterface): Promise<Destructor> {
     // Avoid loading when not in dev mode.
     const { mount } = await import("@latticexyz/dev-tools")
 
-    // contracts need updating as well?
     return (
         (await mount({
             config: mudConfig,
